366a176284a5c434ca71f6dc3224ae55f8e0e70e
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-01-12 13:05:51 +0700

    U(UserServiceImpl): Cap nhat cach log loi

---
+++ src/main/java/com/englishweb/h2t_backside/dto/ErrorDTO.java
@@ -0,0 +1,17 @@
+package com.englishweb.h2t_backside.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+    private String message;
+    private String errorCode;
+    private LocalDateTime timestamp;
+    private Object data;
+}
--- src/main/java/com/englishweb/h2t_backside/service/impl/UserServiceImpl.java
+++ src/main/java/com/englishweb/h2t_backside/service/impl/UserServiceImpl.java
@@ -1,21 +1,30 @@
 package com.englishweb.h2t_backside.service.impl;
 
+import com.englishweb.h2t_backside.dto.ErrorDTO;
 import com.englishweb.h2t_backside.dto.UserDTO;
 import com.englishweb.h2t_backside.exception.ErrorApiCodeContent;
 import com.englishweb.h2t_backside.model.User;
 import com.englishweb.h2t_backside.repository.UserRepository;
 import com.englishweb.h2t_backside.service.DiscordNotifier;
 import com.englishweb.h2t_backside.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
 public class UserServiceImpl extends BaseServiceImpl<UserDTO, User, UserRepository> implements UserService {
 
-    public UserServiceImpl(UserRepository repository, DiscordNotifier discordNotifier) {
+    private final DiscordNotifier discordNotifier;
+    private final ObjectMapper objectMapper;
+
+    public UserServiceImpl(UserRepository repository, DiscordNotifier discordNotifier, ObjectMapper objectMapper) {
         this.repository = repository;
         this.discordNotifier = discordNotifier;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -38,7 +47,19 @@ public class UserServiceImpl extends BaseServiceImpl<UserDTO, User, UserReposito
         String errorMessage = String.format("User with ID '%d' not found.", id);
         log.warn(errorMessage);
 
-        String discordPayload = String.format("Timestamp: %s\nError code: %s\nError message: %s\nError Data: %s", System.currentTimeMillis(), ErrorApiCodeContent.USER_NOT_FOUND, errorMessage, id.toString());
-        discordNotifier.sendNotification(discordPayload);
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .message(errorMessage)
+                .errorCode(ErrorApiCodeContent.USER_NOT_FOUND)
+                .timestamp(LocalDateTime.now())
+                .data(id)
+                .build();
+
+        try {
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            String discordPayload = objectMapper.writeValueAsString(errorDTO);
+            discordNotifier.sendNotification("```json\n" + discordPayload + "\n```");
+        } catch (Exception e) {
+            log.error("Error converting ErrorDTO to JSON: ", e);
+        }
     }
 }
